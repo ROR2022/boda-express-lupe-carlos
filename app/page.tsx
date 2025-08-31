aa432d443c0c100f88d9c32f4b4fe19165d426bf
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2025-08-30 19:59:43 -0600

    basic version

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BodaExpress from '@/components/sections/express/boda/BodaExpress'
+
+const page = () => {
+  return (
+    <div>
+      <BodaExpress />
+    </div>
+  )
+}
+
+export default page
---
+++ components/sections/express/boda/dataBodaExpress.ts
@@ -0,0 +1,20 @@
+//import { babelIncludeRegexes } from "next/dist/build/webpack-config";
+
+
+export const dataBodaExpress = {
+  title: "Nuestra Boda",
+  description: "Invitación de Boda",
+  groom: "Carlos Alberto González Espinoza",
+  bride: "María Guadalupe Picazo Picazo",
+  confirmationNumber: "5217761113708", //+52 1 776 111 3708
+  date: "28 de Diciembre 2025",
+  locationCeremony: "Salón de Eventos Elegance",
+  ceremonyTime: "14:00",
+  ubiLinkCeremony: "https://maps.app.goo.gl/ULbwGJPE8iWVY6hn7",
+  locationReception: "Salón de Eventos Elegance",
+  receptionTime: "15:00",
+  ubiLinkReception: "https://maps.app.goo.gl/ULbwGJPE8iWVY6hn7",
+  guests: 100,
+  music: "/audio/musica.mp3",
+  backgroundImage: "/images/lupe&carlos.jpg"
+}
